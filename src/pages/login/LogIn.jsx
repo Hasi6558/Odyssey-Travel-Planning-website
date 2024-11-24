cb35abd40b5f160a00ecdd76c5e6bb884febb7ee
hasindu <hasindus48@gmail.com>
2024-11-24 21:18:49 +0530

    login page frontend complete

--- src/pages/login/LogIn.jsx
+++ src/pages/login/LogIn.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Logo from '../../assets/images/logo.png'
 import LoginBackground from '../../assets/images/background_login.png'
-import './Login.css'
+import '../../style/Style.css'
+import BlueButton1 from '../../component/Buttons/BlueButton1'
+import InputFieldSet from '../../component/fieldSets/InputFieldSet'
+import GoogleLogo from '../../assets/images/googleLogo.png'
+import FbLogo from '../../assets/images/fbLogo.png'
+import AppleLogo from '../../assets/images/appleLogo.png'
+
+import SocialMediaButton from '../../component/Buttons/SocialMediaButton'
 
 function LogIn() {
   return (
@@ -10,50 +17,34 @@ function LogIn() {
     <div className="login-form">
         <img src={Logo} alt="logo" className='logo' />
         <div className='form-container'>
-            <form action="">
+            <form action="" >
 
-            <p>Start your journey</p>
-            <p>Sign in to Odyssey</p>
+            <p className='login-subtitle' align="left">Start your journey</p>
+            <p className='login-title' align="left">Sign in to Odyssey</p>
 
-            <fieldset className='input-fieldset'>
-            <legend>Username</legend>
-                <div className="input-container">
-                <input type="text" placeholder='example@gmail.com'/>
-                <i class="fa-regular fa-envelope"></i>
-                </div>
-            </fieldset>
-
-
-            <fieldset className='input-fieldset'>
-            <legend>Username</legend>
-                <div className="input-container">
-                <input type="password" placeholder='************'/>
-                <i class="fa-regular fa-eye"></i>
-                </div>
-            </fieldset>
-
-            <button className='sign-in-btn'>
-                Sign In
-            </button>
-
+            
+            <InputFieldSet legend="Email" type="text" iconClass="fa-regular fa-envelope" placeholder="example@gmail.com"/>
+            <br />
+            <InputFieldSet legend="password" type="password" iconClass="fa-regular fa-eye" placeholder="**********"/>
+            <br />
+            <BlueButton1 name='Sign in' className="blueButton1 sign-in-btn"/>
+            <br /><br />
             <fieldset className='social-media-fieldset'>
-                <legend align= "center">or sign in with</legend>
+                <legend align= "center">or sign in with</legend><br />
                 <div className='social-media-sign-in'>
-                    <button>
-                    <i class="fa-brands fa-google"></i>
-                    </button>
+                    <SocialMediaButton src={GoogleLogo}/>
+                    <SocialMediaButton src={FbLogo}/>
+                    <SocialMediaButton src={AppleLogo}/>
                 </div>
             </fieldset>
             
 
             </form>
-           
-
-
         </div>
+        <p>Have not account? <span className='sign-up-link'>Sign Up</span></p>
     </div>
     <div className="login-back-image">
-        <img src={LoginBackground} alt="login background"/>
+        <img src={LoginBackground} alt="" />
     </div>
     </div>
      
