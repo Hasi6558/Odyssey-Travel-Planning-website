11b954802f9340aeb4a5aaa009f37e74b1237b5d
hasindu <hasindus48@gmail.com>
2024-12-28 23:24:05 +0530

    made home and navigation bar responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,18 +9,15 @@ const Home = () => {
       <NavBar />
 
       {/* Top Hotels */}
-
-      <div className='m-20'>
-        <div className='mb-10'>
+      <div className='m-auto w-9/12'>
+        <div className='my-10'>
           <div className='flex items-center justify-between mb-10'>
             <span className='text-4xl font-semibold'>Top Hotels</span>
             <span><a href="#">See all</a></span>
           </div>
 
-          <div className='flex justify-between'>
-            <ContentCard RatingCount={252} price={22} title="Overlooking the Indian Ocean, 
-            Shangri-La Hotel offers" location_city="Colombo" destination_link="#" />
-
+          {/* Responsive Grid */}
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
             <ContentCard RatingCount={252} price={22} title="Overlooking the Indian Ocean, 
             Shangri-La Hotel offers" location_city="Colombo" destination_link="#" />
 
@@ -34,21 +31,18 @@ const Home = () => {
             Shangri-La Hotel offers" location_city="Colombo" destination_link="#" />
           </div>
         </div>
-
       </div>
 
       {/* Popular Restaurants */}
-      <div className='m-20'>
+      <div className='m-auto w-9/12'>
         <div className='mb-10'>
           <div className='flex items-center justify-between mb-10'>
             <span className='text-4xl font-semibold'>Popular Restaurants</span>
             <span><a href="#">See all</a></span>
           </div>
 
-          <div className='flex justify-between'>
-            <ContentCard RatingCount={252} price={22} title="Overlooking the Indian Ocean, 
-            Shangri-La Hotel offers" location_city="Colombo" destination_link="#" />
-
+          {/* Responsive Grid */}
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
             <ContentCard RatingCount={252} price={22} title="Overlooking the Indian Ocean, 
             Shangri-La Hotel offers" location_city="Colombo" destination_link="#" />
 
@@ -62,22 +56,18 @@ const Home = () => {
             Shangri-La Hotel offers" location_city="Colombo" destination_link="#" />
           </div>
         </div>
-
       </div>
 
-
       {/* Best Tours */}
-      <div className='m-20'>
+      <div className='m-auto w-9/12'>
         <div className='mb-10'>
           <div className='flex items-center justify-between mb-10'>
             <span className='text-4xl font-semibold'>Best Tours</span>
             <span><a href="#">See all</a></span>
           </div>
 
-          <div className='flex justify-between'>
-            <ContentCard RatingCount={252} price={22} title="Overlooking the Indian Ocean, 
-            Shangri-La Hotel offers" location_city="Colombo" destination_link="#" />
-
+          {/* Responsive Grid */}
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
             <ContentCard RatingCount={252} price={22} title="Overlooking the Indian Ocean, 
             Shangri-La Hotel offers" location_city="Colombo" destination_link="#" />
 
@@ -91,11 +81,7 @@ const Home = () => {
             Shangri-La Hotel offers" location_city="Colombo" destination_link="#" />
           </div>
         </div>
-
       </div>
-
-
-
 
       <Footer />
     </div>
