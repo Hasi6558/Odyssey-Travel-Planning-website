6d1a0052e1d29eda785f9c5a90df49552859a105
hasindu <hasindus48@gmail.com>
2025-01-04 14:56:50 +0530

    search section created

--- src/component/bars/SearchBar.jsx
+++ src/component/bars/SearchBar.jsx
@@ -4,14 +4,25 @@ import Searchlogo from '../../assets/icons/material-symbols_search.png'
 
 const SearchBar = () => {
     return (
-        <div className='w-screen h-screen  flex justify-center relative bg-cover bg-center' style={{ backgroundImage: "url('https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D')" }}>
-            <style></style>
-            <div className='flex items-center w-2/3 border-8 h-14 border-gray-400 rounded-3xl bg-white p-2'>
-                <span>
+        <div className='w-screen h-screen  flex flex-col items-center relative bg-cover bg-center' style={{ backgroundImage: "url('https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D')" }}>
+
+            <div className='absolute inset-0 bg-black opacity-50'>
+
+            </div>
+
+            <div className='relative'>
+                <h1 className='text-4xl text-white mt-40 font-semibold'>Find Your Perfect Stay </h1>
+
+            </div>
+            <div className='relative'>
+                <p className='text-white text-center my-4 pt-1' >Explore a world of comfort and luxury.<br /> From cozy retreats to grand escapes, discover the ideal destination for your next adventure.</p>
+            </div>
+            <div className='flex items-center w-3/6 border-8 h-16 border-gray-400 rounded-full bg-white p-2 relative mt-10'>
+                <span className='px-2'>
                     <img src={redLocationLogo} alt="" />
                 </span>
                 <input type="text" />
-                <div className='flex items-center'>
+                <div className='flex items-center px-2'>
                     <button><img src={Searchlogo} alt="" className='bg-blue-400 rounded-full p-2' /></button>
                 </div>
             </div >
