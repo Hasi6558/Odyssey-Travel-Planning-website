99e6c3298700412e49e0bb565ae3d57a8f1fb65a
hasindu <hasindus48@gmail.com>
2025-01-04 19:00:22 +0530

    add props to ListingCard

--- src/component/bars/SearchBar.jsx
+++ src/component/bars/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import redLocationLogo from '../../assets/icons/mdi_location_red.png'
 import Searchlogo from '../../assets/icons/material-symbols_search.png'
 
-const SearchBar = ({ title, subtitleLine1, subtitleLine2, hintText }) => {
+const SearchBar = ({ title, subtitleLine1, subtitleLine2, hintText, }) => {
     return (
         <div className='w-screen h-screen  flex flex-col items-center relative bg-cover bg-center' style={{ backgroundImage: "url('https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D')" }}>
 
--- src/component/cards/ListingCard.jsx
+++ src/component/cards/ListingCard.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
 import LocationIcon from '../../assets/icons/location_logo_2.png'
 
-const ListingCard = () => {
+const ListingCard = ({ title, location_city, location_map_url, rating, review_count, description, imgUrl }) => {
     return (
 
-        <div className='rounded-2xl overflow-hidden flex border max-w-[700px] m-2'>
+        <div className='rounded-2xl overflow-hidden flex border max-w-[800px] m-2'>
             <div>
-                <img src="https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D" alt="listing image" className='h-full w-full object-cover  w-[450px] ' />
+                <img src={imgUrl} alt="listing image" className='h-full w-full object-cover w-[350px] ' />
             </div>
-            <div className='w-[800px] p-8'>
-                <h3 className='font-bold text-lg mb-6'>Araliya Red - Lean Luxury - Where you
-                    find stunning 360 panoramic view of
-                    Nuwara Eliya</h3>
+            <div className='w-[700px] p-8'>
+                <h3 className='font-bold text-lg mb-6'>{title}</h3>
                 <div className='flex items-center justify-between overflow-hidden  mb-4'>
                     <div className='flex items-center text'>
                         <span><img src={LocationIcon} alt="" className='h-4' /></span>
-                        <span><p className='pe-8'>Nuwara Eliya</p></span>
-                        <a href="#"><span className='pe-10 font-semibold'>Show on map</span></a>
+                        <span><p className='pe-8 ps-1 '>{location_city}</p></span>
+                        <a href={location_map_url}><span className='pe-10 font-semibold'>Show on map</span></a>
                     </div>
                     <div className='flex flex-col items-center pe-4'>
-                        <div className='bg-blue-700 text-white p-2'><p>4.5</p></div>
-                        <div><p className='text-gray-500'>299 reviews</p></div>
+                        <div className='bg-blue-700 text-white p-2'><p>{rating}</p></div>
+                        <div><p className='text-gray-500'>{review_count} Reviews</p></div>
                     </div>
                 </div>
 
                 <div className='h-24 overflow-hidden'>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt totam sequi pariatur a, iste laboriosam id veniam nisi odio! Porro iure sit maiores dolor in quos quae, harum provident impedit!</p>
+                    <p>{description}</p>
                 </div>
 
 
