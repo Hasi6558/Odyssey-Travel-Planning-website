044ab8d33c6cda58e4105cdaf439a5398d6c6537
Thusithakit <thusitha@kongcepts.info>
2025-01-22 07:27:14 +0530

    changed base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  // base: '/Odyssey-Travel-Planning-website/',
+  // base: '/',
+  base: '/Odyssey-Travel-Planning-website/',
 })
