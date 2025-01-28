47842325e43348bfc5206ab533064942ee6f0dfe
Hasindu Liyanage <you@example.com>
2025-01-28 21:23:15 +0530

    created UI foe add review pop up box

--- src/component/cards/AddReview.jsx
+++ src/component/cards/AddReview.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Star } from "lucide-react";
 
-const AddReview = () => {
+const AddReview = ({handlePopUp}) => {
+    const [rating,setRating] =useState(0);
+    const [hover,setHover] = useState(0);
+
+
   return (
-    <div>
-        AddReview
+    <div className='fixed flex justify-center items-center p-8 inset-0'>
+        <div className='bg-white shadow-md w-[800px] h-[570px] rounded-2xl '>
+           <div className='p-4'>
+            <h1 className='font-bold text-2xl mb-2'>Add Review</h1>   
+            <hr />
+            <div className='text-center text-2xl mt-8 mb-2'>
+            <h1>How would you rate this order ?</h1>
+            </div>
+            <div className='flex justify-center mb-6'>
+            {
+                Array.from({length:5},(_,index)=>{
+                    const starValue =index+1;
+                    return(
+                        <Star
+                        key={starValue}
+                        className={`w-10 h-10 cursor-pointer ${
+                            starValue<=(hover||rating)
+                            ?"text-yellow-400"
+                            :"text-gray-300"
+                    
+                        }`}
+                        onClick={()=>setRating(starValue)}
+                        onMouseEnter={()=>setHover(starValue)}
+                        onMouseLeave={()=>setHover(0)}
+                        />
+                    )
+                })
+            }
+            </div>
+            <div className='flex justify-center items-center'>
+                <textarea name="" id="" className='w-full mx-8 rounded-lg mt-4 p-4 border border-black solid border-2 outline-none' rows={9} >
+
+                </textarea>
+                
+            </div>
+          
+          <div className='flex justify-end me-8 mt-2'>
+            <button className='bg-black text-white p-2 rounded-lg mt-4 me-2 ' onClick={handlePopUp}>
+                Cancel
+            </button>
+            <button className='bg-blue-700 text-white p-2 rounded-lg mt-4 '>
+                Submit
+            </button>
+          </div>
+
+
+           </div>
         </div>
+    </div>
   )
 }
 
--- src/component/cards/PurchaseCard.jsx
+++ src/component/cards/PurchaseCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const PurchaseCard = () => {
+const PurchaseCard = ({handlePopUp}) => {
     return (
-        <div className='border rounded-xl p-4 bg-white'>
+        <div className='border rounded-xl px-8 py-4 bg-white'>
             <div className='flex flex-col items-end '>
                 <div className=''>Order Id: <span>55545565425456456445</span></div>
                 <div>Date : 2025/01/01 00:00:00 </div>
@@ -11,10 +11,10 @@ const PurchaseCard = () => {
             <div className='flex justify-between'>
             <div className='flex flex-col justify-between'>
                 <div className='mt-4'><h1 className='text-2xl font-semibold '>Unwind in Unmatched Comfort at Ocean Pearl</h1></div>
-                <div className='text-xl mt-1 font-semibold'><p>Total :240 $ </p></div>
+                <div className='text-xl mt-1 font-semibold mb-2'><p>Total :240 $ </p></div>
             </div>
-            <div className='flex flex-col justify-center items-center m-2'>
-                <button className='text-white font-semibold p-2 bg-blue-700 rounded-lg'>+ Add a review</button>
+            <div className='flex flex-col justify-center items-center m-2 my-4'>
+                <button className='text-white font-semibold p-2 bg-blue-700 rounded-lg mb-2' onClick={handlePopUp}>+ Add a review</button>
                  <div className='mt-1 font-semibold text-white bg-black p-2 rounded-lg w-full text-center'><p><a href="">Show Item </a></p></div>
             </div>
             </div>
